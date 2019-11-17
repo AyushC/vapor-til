25716c084467b591f4cf30eba24a9ab82e599776
Ayush Chamoli <ayush.chamoli13@gmail.com>
2019-11-16 19:44:20 -0800

    Add basic website

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,5 +1,6 @@
 import FluentPostgreSQL
 import Vapor
+import Leaf
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
@@ -22,11 +23,27 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     let hostname = Environment.get("DATABASE_HOSTNAME")
         ?? "localhost"
     let username = Environment.get("DATABASE_USER") ?? "vapor"
-    let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+    
+    let databaseName: String
+    let databasePort: Int
+    
+    if (env == .testing) {
+        databaseName = "vapor-test"
+    } else {
+        databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+    }
+    
+    if let testPort = Environment.get("DATABASE_PORT") {
+        databasePort = Int(testPort) ?? 5433
+    } else {
+        databasePort = 5432
+    }
+    
     let password = Environment.get("DATABASE_PASSWORD")
         ?? "password"
     let databaseConfig = PostgreSQLDatabaseConfig(
         hostname: hostname,
+        port: databasePort,
         username: username,
         database: databaseName,
         password: password)
@@ -45,4 +62,8 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     var commandConfig = CommandConfig.default()
     commandConfig.useFluentCommands()
     services.register(commandConfig)
+    
+    try services.register(LeafProvider())
+    
+    config.prefer(LeafRenderer.self, for: ViewRenderer.self)
 }
