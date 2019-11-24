49202b4c9ada1ae53e9308b916d8ebcf6d8b6ced
Ayush Chamoli <ayush.chamoli13@gmail.com>
2019-11-24 14:23:54 -0800

    update swift version for docker

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -41,7 +41,7 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
             }
         } else {
             databaseName = Environment.get("DATABASE_DB") ?? "vapor"
-            databasePort = 5433
+            databasePort = 5432
         }
         
         databaseConfig = PostgreSQLDatabaseConfig(
