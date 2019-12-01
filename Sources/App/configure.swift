c8c3f227945cca13b1aa2f8d67ef8d8158294c94
Ayush Chamoli <ayush.chamoli13@gmail.com>
2019-12-01 15:04:23 -0800

    configured heroku database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,19 +7,19 @@ import Authentication
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     // Register providers first
     try services.register(FluentPostgreSQLProvider())
-
+    
     // Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
     services.register(router, as: Router.self)
-
+    
     // Register middleware
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
     middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     middlewares.use(SessionsMiddleware.self)
     services.register(middlewares)
-
+    
     try services.register(LeafProvider())
     try services.register(AuthenticationProvider())
     
@@ -59,7 +59,7 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     let database = PostgreSQLDatabase(config: databaseConfig)
     databases.add(database: database, as: .psql)
     services.register(databases)
-
+    
     // Configure migrations
     var migrations = MigrationConfig()
     migrations.add(model: User.self, database: .psql)
